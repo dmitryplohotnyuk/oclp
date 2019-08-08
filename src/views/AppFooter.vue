b2cc5fcf4576d92d047337d6aaab5689262ff92d
Dmitry Plokhotniuk <dmitry.plohotnyuk@gmail.com>
2019-08-08 21:04:51 +0300

    Init

---
+++ src/views/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-footer absolute app dark class="primary">
+      <v-flex text-xs-center>
+        <span>&copy; OCLP Monitor</span>
+      </v-flex>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ src/views/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-toolbar app dark class="primary">
+      <v-toolbar-title>
+        <v-icon>mdi-atom</v-icon>
+        ПАО "КСЗ"
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn to="/" class="primary">
+           Главная
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
